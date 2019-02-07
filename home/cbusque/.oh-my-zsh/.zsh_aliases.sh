934f0d066c09d773291e8d0e0b741aac5fa16c82
cedrick.busque@globaleagle.com <cedrick.busque@globaleagle.com>
2019-02-07 14:38:34 -0500

    update

--- home/cbusque/.oh-my-zsh/.zsh_aliases.sh
+++ home/cbusque/.oh-my-zsh/.zsh_aliases.sh
@@ -110,6 +110,7 @@ aliase()
 vim ~/.oh-my-zsh/.zsh_aliases.sh
 source ~/.oh-my-zsh/.zsh_aliases.sh
 diff ~/.oh-my-zsh/.zsh_aliases.sh ~/gee/home/cbusque/.oh-my-zsh/.zsh_aliases.sh
+echo $status
 if [ "$status" != "true" ]
 then
 cp ~/.oh-my-zsh/.zsh_aliases.sh ~/gee/home/cbusque/.oh-my-zsh/.zsh_aliases.sh

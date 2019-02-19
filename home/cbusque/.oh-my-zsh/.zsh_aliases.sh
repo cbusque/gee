cb9cfa5138144444c8a193fcdfb44b58c020e761
cedrick.busque@globaleagle.com <cedrick.busque@globaleagle.com>
2019-02-19 15:25:18 -0500

    update

--- home/cbusque/.oh-my-zsh/.zsh_aliases.sh
+++ home/cbusque/.oh-my-zsh/.zsh_aliases.sh
@@ -180,4 +180,7 @@ scp row44@192.168.80.$1:/mnt/media_a/__devwork__/cbusque/vc/$2/\*.tar.bz2 ~/vc/$
 VC="$(ls -t ~/vc/$2 | head -1)"
 scp ~/vc/$2/$VC util7:~ ;
 #to improve: select a good tail and vcdiff on it
+ssh util7 << EOF
+longsm ^N | sort -nrk 3 | egrep  ' 0%' | tail -n 1 | awk '{print $1}' | tr -d : ;
+EOF
 }

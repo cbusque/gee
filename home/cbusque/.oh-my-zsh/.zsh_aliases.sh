90ebbd2b38c39dcd9185510b358bf05a72722f88
cedrick.busque@globaleagle.com <cedrick.busque@globaleagle.com>
2019-02-26 14:46:09 -0500

    update

--- home/cbusque/.oh-my-zsh/.zsh_aliases.sh
+++ home/cbusque/.oh-my-zsh/.zsh_aliases.sh
@@ -35,7 +35,7 @@ sw()
 }
 showimage()
 {
-	#showimage "$(whoami)" util7 '~/Chase_444_generic_July18_LB_LT_1458x180.jpg'
+	#showimage cbusque util7 '~/Chase_444_generic_July18_LB_LT_1458x180.jpg'
 ssh $1@$2 "cat $3" | display
 }
 sshpoly()
@@ -60,7 +60,7 @@ echo "0303871022   TnvL8BPWzT	Test	Aia	 adalbolafs	          baramella@gmail.com
 mumbai()
 {
 	echo "su password : jai@9w!"
-ssh -tt "$(whoami)"@util7 "ssh jai@smu-000150g"
+ssh -tt cbusque@util7 "ssh jai@smu-000150g"
 }
 ACE()
 {
@@ -77,7 +77,7 @@ gee-disconnect-vpn.sh
 gee-connect-vpn.sh
 snx -d
 stoken
-snx -s 67.142.235.252 -u "$(whoami)"
+snx -s 67.142.235.252 -u cbusque
 }
 stopsnx()
 {
@@ -86,15 +86,15 @@ snx -d
 startsnx()
 {
 snx -d
-echo '1234' | stoken | sed 's/.*\(........\)/\1/' | snx -s 67.142.235.252 -u "$(whoami)"
+echo '1234' | stoken | sed 's/.*\(........\)/\1/' | snx -s 67.142.235.252 -u cbusque
 #stoken
-#snx -s 67.142.235.252 -u "$(whoami)"
+#snx -s 67.142.235.252 -u cbusque
 }
 snxeu()
 {
 snx -d;
 stoken;
-snx -s eu.row44.com -u "$(whoami)";
+snx -s eu.row44.com -u cbusque;
 }
 view_tail(){
 #TAILNAME=$1
@@ -109,38 +109,38 @@ aliase()
 {
 vim ~/.oh-my-zsh/.zsh_aliases.sh
 source ~/.oh-my-zsh/.zsh_aliases.sh
-diff ~/.oh-my-zsh/.zsh_aliases.sh ~/gee/home/"$(whoami)"/.oh-my-zsh/.zsh_aliases.sh > /dev/null
+diff ~/.oh-my-zsh/.zsh_aliases.sh ~/gee/home/cbusque/.oh-my-zsh/.zsh_aliases.sh > /dev/null
 if [ "$status" = 1 ]
 then
-cp ~/.oh-my-zsh/.zsh_aliases.sh ~/gee/home/"$(whoami)"/.oh-my-zsh/.zsh_aliases.sh
-git -C ~/gee/home/"$(whoami)"/.oh-my-zsh/ add ~/gee/home/"$(whoami)"/.oh-my-zsh/.zsh_aliases.sh
-git -C ~/gee/home/"$(whoami)"/.oh-my-zsh/ commit -m 'update'
-git -C ~/gee/home/"$(whoami)"/.oh-my-zsh/ push
+cp ~/.oh-my-zsh/.zsh_aliases.sh ~/gee/home/cbusque/.oh-my-zsh/.zsh_aliases.sh
+git -C ~/gee/home/cbusque/.oh-my-zsh/ add ~/gee/home/cbusque/.oh-my-zsh/.zsh_aliases.sh
+git -C ~/gee/home/cbusque/.oh-my-zsh/ commit -m 'update'
+git -C ~/gee/home/cbusque/.oh-my-zsh/ push
 fi
 }
 sshconf()
 {
 
 vim ~/.ssh/config
-diff ~/.ssh/config ~/gee/home/"$(whoami)"/.ssh/config
+diff ~/.ssh/config ~/gee/home/cbusque/.ssh/config
 if [ "$status" = 1 ]
 then
-cp ~/.ssh/config ~/gee/home/"$(whoami)"/.ssh/config
-git -C  ~/gee/home/"$(whoami)"/ add ~/gee/home/"$(whoami)"/.ssh/config
-git -C  ~/gee/home/"$(whoami)"/ commit -m 'update'
-git -C  ~/gee/home/"$(whoami)"/ push
+cp ~/.ssh/config ~/gee/home/cbusque/.ssh/config
+git -C  ~/gee/home/cbusque/ add ~/gee/home/cbusque/.ssh/config
+git -C  ~/gee/home/cbusque/ commit -m 'update'
+git -C  ~/gee/home/cbusque/ push
 fi
 }
 bashrig()
 {
 vim .bash_navigation
-diff ~/.bash_navigation ~/gee/home/"$(whoami)"/.bash_navigation
+diff ~/.bash_navigation ~/gee/home/cbusque/.bash_navigation
 if [ "$status" = 1 ]
 then
-cp ~/.bash_navigation ~/gee/home/"$(whoami)"/.bash_navigation
-git -C cd ~/gee/home/"$(whoami)"/ add ~/gee/home/"$(whoami)"/.bash_navigation
-git -C cd ~/gee/home/"$(whoami)"/ commit -m 'update'
-git -C cd ~/gee/home/"$(whoami)"/ push
+cp ~/.bash_navigation ~/gee/home/cbusque/.bash_navigation
+git -C cd ~/gee/home/cbusque/ add ~/gee/home/cbusque/.bash_navigation
+git -C cd ~/gee/home/cbusque/ commit -m 'update'
+git -C cd ~/gee/home/cbusque/ push
 fi
 }
 pbcopy()
@@ -150,10 +150,10 @@ xclip -selection c
 rig ()
 {
     server="root@192.168.80.$1";
-    ssh -q $server -t "[ -e /mnt/media_a/__devwork__/"$(whoami)"/ ] || mkdir -p /mnt/media_a/__devwork__/"$(whoami)"/";
-    scp ~/.bash_navigation $server:/mnt/media_a/__devwork__/"$(whoami)"/ || return 1;
+    ssh -q $server -t "[ -e /mnt/media_a/__devwork__/cbusque/ ] || mkdir -p /mnt/media_a/__devwork__/cbusque/";
+    scp ~/.bash_navigation $server:/mnt/media_a/__devwork__/cbusque/ || return 1;
     echo -e "Connection to $1 requested...";
-    ssh -q $server -t "cd /home/row44;export PATH=/usr/local/row44/utils/bin/:$PATH;/bin/bash" --rcfile /mnt/media_a/__devwork__/"$(whoami)"/.bash_navigation 
+    ssh -q $server -t "cd /home/row44;export PATH=/usr/local/row44/utils/bin/:$PATH;/bin/bash" --rcfile /mnt/media_a/__devwork__/cbusque/.bash_navigation 
 }
 util7(){
 ssh util7
@@ -161,10 +161,10 @@ ssh util7
 ovh()
 {
   server="$1"
-  ssh -q $server -t "[ -e /mnt/media_a/__devwork__/"$(whoami)"/ ] || mkdir -p /mnt/media_a/__devwork__/"$(whoami)"/";
-    scp ~/.bash_navigation $server:/mnt/media_a/__devwork__/"$(whoami)"/ || return 1;
+  ssh -q $server -t "[ -e /mnt/media_a/__devwork__/cbusque/ ] || mkdir -p /mnt/media_a/__devwork__/cbusque/";
+    scp ~/.bash_navigation $server:/mnt/media_a/__devwork__/cbusque/ || return 1;
     echo -e "Connection to ovh $1 requested...";
-    ssh -q $server -t "cd /home/row44;/bin/bash" --rcfile /mnt/media_a/__devwork__/"$(whoami)"/.bash_navigation
+    ssh -q $server -t "cd /home/row44;/bin/bash" --rcfile /mnt/media_a/__devwork__/cbusque/.bash_navigation
 }
 ovh_list()
 {
@@ -183,12 +183,12 @@ vcdiff(){
 # 	pour ICE => T
 # https://confluence.geemedia.com/display/OPSUPPORT/Airline+cheat+sheet
 mkdir ~/vc/$2
-scp row44@192.168.80.$1:/mnt/media_a/__devwork__/"$(whoami)"/vc/$2/\*.tar.bz2 ~/vc/$2 ;
+scp row44@192.168.80.$1:/mnt/media_a/__devwork__/cbusque/vc/$2/\*.tar.bz2 ~/vc/$2 ;
 VC="$(ls -t ~/vc/$2 | head -1)"
 scp ~/vc/$2/$VC util7:~ ;
 #to improve: select a good tail and vcdiff on it
 ssh util7 << EOF
-TAIL="\$(/home/"$(whoami)"/bin/besttail $3)" ;
+TAIL="\$(/home/cbusque/bin/besttail $3)" ;
 echo \$TAIL ;
 THEDIFF="\$(vcdiff ~/$VC \${TAIL}g)";
 echo \$THEDIFF ;
